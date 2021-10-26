dc9051d4f7da314b2e27a8ef3c6b7e3ac437ed4f
PaaavelZ <paaavel.2024@gmail.com>
2021-10-26 23:05:23 +0300

    Make some cosmetic changes

--- Technopolis/src/index/cosmetic/change_theme.js
+++ Technopolis/src/index/cosmetic/change_theme.js
@@ -1,37 +1,52 @@
-document.getElementById("card__change-theme__slider")
-    .addEventListener("click", changeTheme);
+const slider = document.getElementById("card__change-theme__slider");
+slider.addEventListener("click", changeTheme);
 
 const N_PROPS = 6;
 const PROPS_NAMES = ["-left-side-main-theme", "-right-side-main-theme",
     "-font-color", "-border-color", "-theme-change-strip-color",
     "-avatar-shadow-color"];
-const DEFAULT_PREFIX = "--default";
-const LIGHT_PREFIX = "--light";
 
+/* ======Default properties====== */
+const DEFAULT_PREFIX = "--default";
 const defaultThemePropsNames = new Array(N_PROPS);
 for (let i = 0; i < N_PROPS; ++i) {
     defaultThemePropsNames[i] = DEFAULT_PREFIX + PROPS_NAMES[i];
 }
 
+const defaultThemePropsValues = new Array(N_PROPS);
+for (let i = 0; i < N_PROPS; ++i) {
+    defaultThemePropsValues[i] = getComputedStyle(document.documentElement)
+        .getPropertyValue(defaultThemePropsNames[i]);
+}
+
+/* ======Light properties====== */
+const LIGHT_PREFIX = "--light";
 const lightThemePropsNames = new Array(N_PROPS);
 for (let i = 0; i < N_PROPS; ++i) {
     lightThemePropsNames[i] = LIGHT_PREFIX + PROPS_NAMES[i];
 }
 
+const lightThemePropsValues = new Array(N_PROPS);
+for (let i = 0; i < N_PROPS; ++i) {
+    lightThemePropsValues[i] = getComputedStyle(document.documentElement)
+        .getPropertyValue(lightThemePropsNames[i]);
+}
+
+/* ======Click processing====== */
 let isDefaultTheme = true;
 function changeTheme() {
     if (isDefaultTheme) {
         for (let i = 0; i < N_PROPS; ++i) {
             document.documentElement.style.setProperty(defaultThemePropsNames[i],
-                getComputedStyle(document.documentElement)
-                    .getPropertyValue(lightThemePropsNames[i]));
+                lightThemePropsValues[i]);
         }
+        slider.style.setProperty("left", "0%");
     } else {
         for (let i = 0; i < N_PROPS; ++i) {
             document.documentElement.style.setProperty(defaultThemePropsNames[i],
-                getComputedStyle(document.documentElement)
-                    .getPropertyValue(defaultThemePropsNames[i]));
+                defaultThemePropsValues[i]);
         }
+        slider.style.setProperty("left", "80%");
     }
 
     isDefaultTheme = !isDefaultTheme;
