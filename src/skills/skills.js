fae25234d948a2195eda42b8f3478d8cd93c22d3
PaaavelZ <paaavel.2024@gmail.com>
2021-12-14 16:45:56 +0300

    Add/Fix: 1) Shift to the limits of the skill ratio, if it is outside them; 2) Use DOMParser for readability's increasing; 3) Change addSkillBtn functionality - make it submit element; 4) Use querySelector instead array of elements by className when receiving certain input field value; 5) Simplify skills' reading.

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -1,118 +1,152 @@
+//import {Utils} from "./utils";
+
 const skills = document.getElementById("skills");
+let skillStorage = [];
 
 class Skill {
 
+    static #DOMParser = new DOMParser();
+    static #skillNameInput = document.querySelector(".skills__header__form__name");
+    static #skillRatioInput = document.querySelector(".skills__header__form__ratio");
+
     #skillName;
     #skillRatio;
-    static #skillNameInput = document.getElementsByClassName("skills__header__form__skill-name-input")[0];
-    static #skillRatioInput = document.getElementsByClassName("skills__header__form__skill-ratio-input")[0];
 
     constructor(skillName, skillRatio) {
         this.#skillName = skillName;
         this.#skillRatio = skillRatio;
+        new RegExp()
     }
 
     render(skillIndex, skillRatio) {
         const skill = document.createElement("div");
         skill.id = "skills__" + skillIndex;
-        skill.style.margin = "2% 0 0 12%";
+        if (skillStorage.length === 0)
+            skill.style.margin = "-8% 0 0 12%";
+        else
+            skill.style.margin = "2% 0 0 12%";
 
         this.#renderSkillHeader(skill);
         Skill.#renderSkillProgressSlider(skill, skillRatio);
         Skill.#renderDeleteSkillBtn(skill);
+
         skills.appendChild(skill);
     }
 
     #renderSkillHeader(skill) {
-        const skillHeader = document.createElement("div");
-        skillHeader.style.display = "flex";
-        skillHeader.style.width = "30%";
-        skillHeader.style.justifyContent = "space-between";
-        skillHeader.style.flexDirection = "row";
-        skillHeader.style.fontSize = "1.3em";
-        skillHeader.innerHTML = Skill.#getSkillHeaderInnerHtml(skill.id, this.#skillName, this.#skillRatio);
+        let skillHeaderHTML = Skill.#getSkillHeaderHTML(skill.id, this.#skillName, this.#skillRatio);
+        let skillHeader = Skill.#getNodeFromString(skillHeaderHTML);
         skill.appendChild(skillHeader);
     }
 
-    static #getSkillHeaderInnerHtml(skillID, skillName, skillRatio) {
-        return `<span style="align-self: flex-start">${skillName}</span>
-          <span id="${skillID}__ratio" style="align-self: flex-end">${skillRatio}%</span>`;
+    static #getSkillHeaderHTML(skillID, skillName, skillRatio) {
+        return `
+          <div style="display: flex; justify-content: space-between; flex-direction: row; width: 30%; font-size: 1.3em;">
+            <span>${skillName}</span>
+            <span id="${skillID}__ratio">${skillRatio}%</span>
+          </div>
+        `;
     }
 
     static #renderSkillProgressSlider(skill, skillRatio) {
-        const skillProgress = document.createElement("input");
-        skillProgress.type = "range";
-        skillProgress.style.width = "30%";
-        skillProgress.id = skill.id + "__slider";
-        skillProgress.className = skillProgress.id + " styled-slider slider-progress";
-        skillProgress.style.background = "black";
-        skillProgress.value = skillRatio;
-        skill.appendChild(skillProgress);
-        renderSlider(skillProgress, skill.id);
+        let skillProgressSliderHTML = Skill.#getSkillProgressSliderHTML(skill.id, skillRatio);
+        let skillProgressSlider = Skill.#getNodeFromString(skillProgressSliderHTML);
+        skill.appendChild(skillProgressSlider);
+        renderSlider(skillProgressSlider, skill.id);
+    }
+
+    static #getSkillProgressSliderHTML(skillID, skillRatio) {
+        return `
+          <input type="range" id="${skillID}__slider" class="${skillID}__slider styled-slider slider-progress"
+            value="${skillRatio}" style="width: 30%; background: black">
+        `;
     }
 
     static #renderDeleteSkillBtn(skill) {
-        const deleteSkillButton = document.createElement("input");
-        deleteSkillButton.type = "image";
-        deleteSkillButton.onclick = function() {removeSkill(skill.id)};
-        deleteSkillButton.src = "src/skills/rubbish-bin.svg";
-        deleteSkillButton.style.width = "30px";
-        deleteSkillButton.alt = "skill delete button";
-        deleteSkillButton.style.cursor = "pointer";
-        deleteSkillButton.className = `${skill.id}__delete-skill`;
-        deleteSkillButton.style.margin = "0 0 -0.6% 0";
-        skill.appendChild(deleteSkillButton);
+        let deleteSkillBtnHTML = Skill.#getDeleteSkillBtnHTML(skill);
+        let deleteSkillBtn = Skill.#getNodeFromString(deleteSkillBtnHTML);
+        deleteSkillBtn.onclick = function() {removeSkill(skill.id)};
+        skill.appendChild(deleteSkillBtn);
     }
 
-    get skillRatio() {
-        return this.#skillRatio;
+    static #getDeleteSkillBtnHTML(skill) {
+        return `
+            <input type="image" src="src/skills/rubbish-bin.svg" class="${skill.id}__delete-skill"
+              style="width: 30px; cursor: pointer; margin-bottom: -0.6%;" alt="skill delete button">
+        `;
     }
 
-    static readSkill() {
-        let skillName = this.#readSkillName();
-        let skillRatio = this.#readSkillRatio();
-        if (skillName === undefined || skillRatio === undefined)
-            return undefined;
-        return new Skill(skillName, skillRatio);
+    static #getNodeFromString(str) {
+        return Skill.#DOMParser.parseFromString(str, "text/html").documentElement.childNodes[1].firstChild;
+    }
+
+    static readSkill(skill) {
+        skill.skillName = this.#readSkillName();
+        skill.skillRatio = this.#readSkillRatio();
     }
 
     static #readSkillName() {
         let skillName = Skill.#skillNameInput.value;
+
         if (skillName.length === 0) {
             alert("Skills name wasn't entered. Try again");
             return undefined;
         }
+
         if (skillName.length > Skill.#skillNameInput.size) {
             skillName = skillName.substring(0, Skill.#skillNameInput.size);
             alert("Skills name is very long. It will be shortened to " + skillName);
         }
-        skillName.replaceAll("<", "&lt");
-        skillName.replaceAll(">", "&gt");
+
+        skillName = replaceAll(skillName, "<", "&lt");
+        skillName = replaceAll(skillName, ">", "&gt");
+
+        //Utils.validateSkillNameInput(skillName);
         Skill.#skillNameInput.value = "";
         return skillName;
     }
 
     static #readSkillRatio() {
         let skillRatio = Number.parseInt(Skill.#skillRatioInput.value);
-        let minSkillRatio = Number.parseInt(Skill.#skillRatioInput.min);
-        let maxSkillRatio = Number.parseInt(Skill.#skillRatioInput.max);
-        if (isNaN(skillRatio) || skillRatio < minSkillRatio || skillRatio > maxSkillRatio) {
+        const minSkillRatio = Number.parseInt(Skill.#skillRatioInput.min);
+        const maxSkillRatio = Number.parseInt(Skill.#skillRatioInput.max);
+
+        if (isNaN(skillRatio)) {
             alert("Skills ratio was incorrect entered. Try again");
             skillRatio = undefined;
-
+        } else if (skillRatio < minSkillRatio) {
+            skillRatio = minSkillRatio;
+        } else if (skillRatio > maxSkillRatio) {
+            skillRatio = maxSkillRatio;
         }
+
         Skill.#skillRatioInput.value = "";
         return skillRatio;
     }
 
+    get skillName() {
+        return this.#skillName;
+    }
+
+    set skillName(skillName) {
+        this.#skillName = skillName;
+    }
+
+    get skillRatio() {
+        return this.#skillRatio;
+    }
+
+    set skillRatio(skillRatio) {
+        this.#skillRatio = skillRatio;
+    }
+
 }
 
-let skillStorage = [];
 let isPlugRendered = false;
 const plugDescription = `
       <style>
           #skills__plug {
-            margin: 5% 0 0 10%;
+            margin: 0 0 0 10%;
             font-size: 2em;
           }
       </style>
@@ -120,13 +154,12 @@ const plugDescription = `
 `;
 
 function addSkill() {
-    let skill = Skill.readSkill();
-    if (skill === undefined) {
+    let skill = new Skill(null, null);
+    Skill.readSkill(skill);
+    if (skill.skillRatio === undefined || skill.skillName === undefined)
         return;
-    }
-    if (isPlugRendered) {
+    if (isPlugRendered)
         erasePlug();
-    }
     skill.render(skillStorage.length, skill.skillRatio);
     skillStorage.splice(skillStorage.length, 0, skill);
 }
@@ -136,9 +169,8 @@ function removeSkill(skillID) {
     let removedSkill = document.getElementById(skillID);
     skills.removeChild(removedSkill);
     skillStorage.splice(skillOrdinal, 1);
-    if (skillStorage.length === 0) {
+    if (skillStorage.length === 0)
         renderPlug();
-    }
 }
 
 function renderPlug() {
@@ -155,7 +187,14 @@ function erasePlug() {
     isPlugRendered = false;
 }
 
-document.getElementById("skills__header__add-skill-wrap")
-    .addEventListener("click", addSkill);
+function onAddSkill(event) {
+    event.preventDefault();
+    addSkill();
+}
 
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+document.getElementById("skills__header__form").addEventListener("submit", onAddSkill);
 renderPlug();
